fd906856e2edd3fe716b572b7fa30c312de5b5b5
MikhaelCV <mikhael.canaza@estudiante.ucsm.edu.pe>
2025-03-27 22:34:23 -0500

    chocolatina.java

--- chocolatina.java
+++ chocolatina.java
@@ -18,3 +18,39 @@ public class Chocolatina {
         return "Chocolatina: " + marca;
     }
 }
+
+/*EJERCICIO 6
+public class Chocolatina {
+    private String marca;
+    private double peso; // en gramos
+
+    public Chocolatina(String marca, double peso) {
+        this.marca = marca;
+        this.peso = peso;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Chocolatina that = (Chocolatina) obj;
+        return Double.compare(that.peso, peso) == 0 && marca.equalsIgnoreCase(that.marca);
+    }
+
+    @Override
+    public String toString() {
+        return marca + " (" + peso + "g)";
+    }
+}
+*/
+
+    
+
